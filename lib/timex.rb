8ac8bed4c1eb0047bc6a2c127dfda5572233a9a2
Julian Martinez <julianeon@gmail.com>
2013-05-02 15:19:34 -0500

    Improved w/corrected methods.

--- lib/timex.rb
+++ lib/timex.rb
@@ -1,15 +1,14 @@
 module Timex
 
-  def getdayweek
+  def Timex.getdayweek
     daynum= Time.now.utc.wday
     week={1=> "Monday", 2=> "Tuesday", 3=> "Wednesday", 4=> "Thursday", 5=> "Friday"}
     dayofweek= week[daynum]
   end
 
-  # GET /tasks
-  # GET /tasks.json
-  def thisdate
+  def Timex.thisdate
     time=Time.new
     date=time.month.to_s + "/" + time.day.to_s + "/" + time.year.to_s
   end
+
 end
