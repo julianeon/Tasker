7fc525dda137f8d58867fb47d192cb2715605d92
Julian Martinez <julianeon@gmail.com>
2013-05-03 15:45:47 -0500

    Expanded and included better method names.

--- lib/timex.rb
+++ lib/timex.rb
@@ -1,14 +1,18 @@
 module Timex
 
-  def Timex.getdayweek
+  def Timex.dayweek
     daynum= Time.now.utc.wday
     week={1=> "Monday", 2=> "Tuesday", 3=> "Wednesday", 4=> "Thursday", 5=> "Friday"}
     dayofweek= week[daynum]
   end
 
-  def Timex.thisdate
+  def Timex.date
     time=Time.new
     date=time.month.to_s + "/" + time.day.to_s + "/" + time.year.to_s
   end
 
+  def Timex.dayyear
+    time=Time.new
+    date=time.yday
+  end
 end
