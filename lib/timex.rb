dbed1ea330c46d8dd36f2debe7afaa97d6a4c43a
Julian Martinez <julianeon@gmail.com>
2013-05-04 07:09:49 -0500

    Added support for Sat and Sun.

--- lib/timex.rb
+++ lib/timex.rb
@@ -2,7 +2,7 @@ module Timex
 
   def Timex.dayweek
     daynum= Time.now.utc.wday
-    week={1=> "Monday", 2=> "Tuesday", 3=> "Wednesday", 4=> "Thursday", 5=> "Friday"}
+    week={0=>"Sunday", 1=> "Monday", 2=> "Tuesday", 3=> "Wednesday", 4=> "Thursday", 5=> "Friday", 6=> "Saturday"}
     dayofweek= week[daynum]
   end
 
@@ -12,7 +12,10 @@ module Timex
   end
 
   def Timex.dayyear
-    time=Time.new
-    date=time.yday
+    Time.now.yday
+  end
+  
+  def Timex.stopwatch
+    Time.now.to_i
   end
 end
